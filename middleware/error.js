661bda3336bdbc2dcf39d7028f2e9fb673b60c42
billy <k4k4via@hotmail.com>
2019-05-06 01:40:38 +0300

    added case on error handling

--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,11 @@
 const logger = require("../configuration/logging");
 
 module.exports = (err, req, res, next) => {
-  const { message, status = 400 } = err;
-  logger.error(err.message, err);
+  let { message, status = 400 } = err;
+
+  if (err.name && err.name === "MongoNetworkError")
+    message = "An error occured";
+
+  logger.error(message, err);
   res.status(status).send({ error: message });
 };
