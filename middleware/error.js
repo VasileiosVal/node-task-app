f11e38eb9f8ebb5ea1accf4b0e99b5f68f8fa138
billy <k4k4via@hotmail.com>
2019-05-06 01:52:56 +0300

    fixing bug

--- middleware/error.js
+++ middleware/error.js
@@ -3,9 +3,12 @@ const logger = require("../configuration/logging");
 module.exports = (err, req, res, next) => {
   let { message, status = 400 } = err;
 
-  if (err.name && err.name === "MongoNetworkError")
-    message = "An error occured";
-
   logger.error(message, err);
+
+  if (err.name && err.name === "MongoNetworkError") {
+    message = "An error occured";
+    status = 500;
+  }
+
   res.status(status).send({ error: message });
 };
